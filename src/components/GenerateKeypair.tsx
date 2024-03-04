de741a26288d7c8313366cf9eaab4a16df83904b
alxwnth <alxwnth@posteo.me>
2024-03-04 18:18:28 +0200

    feat: Generating keypair

--- src/components/GenerateKeypair.tsx
+++ src/components/GenerateKeypair.tsx
@@ -1,27 +1,36 @@
+import { createSignal } from "solid-js";
+
+import { bytesToHex } from "@noble/curves/abstract/utils";
+import { ed25519 } from "@noble/curves/ed25519";
+
 const GenerateKeypair = () => {
+  const [privateKey, setPrivateKey] = createSignal("");
+  const [publicKey, setPublicKey] = createSignal("");
+
+  function generateKeypair() {
+    const privateKeyBytes = ed25519.utils.randomPrivateKey();
+    setPrivateKey(bytesToHex(privateKeyBytes));
+    setPublicKey(bytesToHex(ed25519.getPublicKey(privateKeyBytes)));
+  }
 
   return (
     <article>
       <header>Generate keypair</header>
-      <button>Generate</button>
-      <div class="block">
-        <div class="field">
-          <label class="label">Private key</label>
-          <input class="input" v-model="privateKey" disabled />
-        </div>
-        <div class="field">
-          <label class="label">Public key</label>
-          <input class="input" v-model="publicKey" disabled />
-        </div>
-      </div>
-      <div v-if="privateKey !== ''">
-        <div role="group">
-          <button class="">Use these keys</button>
-          <button class="secondary">Download keys</button>
-          <button class="secondary">
-            Clear generated keys
-          </button>
-        </div>
+      <input type="button" value="Generate" onclick={generateKeypair} />
+
+      <label>
+        Private key
+        <input value={privateKey()} disabled />
+      </label>
+      <label>
+        Public key
+        <input value={publicKey()} disabled />
+      </label>
+
+      <div role="group">
+        <button class="">Use these keys</button>
+        <button class="secondary">Download keys</button>
+        <button class="secondary">Clear generated keys</button>
       </div>
       <footer>Be aware! Keys are erased when you restart the page.</footer>
     </article>
