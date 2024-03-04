2c3e937661d1984e0bea71250d79378af4709f48
alxwnth <alxwnth@posteo.me>
2024-03-04 20:00:37 +0200

    feat: Import key pair

--- src/components/ImportKeypair.tsx
+++ src/components/ImportKeypair.tsx
@@ -1,6 +1,12 @@
+import { ed25519 } from "@noble/curves/ed25519";
+import { hexToBytes, bytesToHex } from "@noble/curves/abstract/utils";
+
 import { useKeypairContext } from "../contexts/KeypairContextProvider";
+import { Show, createSignal } from "solid-js";
 
 export default function ImportKeypair() {
+  let privateKeyInputField: HTMLInputElement | undefined;
+  let [privateKeyImportError, setPrivateKeyImportError] = createSignal(false);
   // TODO: Fix types for context
   const {
     // @ts-expect-error
@@ -13,6 +19,20 @@ export default function ImportKeypair() {
     setContextPublicKey,
   } = useKeypairContext();
 
+  function importKeypair() {
+    if (privateKeyInputField) {
+      setContextPrivateKey(privateKeyInputField.value);
+      try {
+        setContextPublicKey(
+          bytesToHex(ed25519.getPublicKey(hexToBytes(contextPrivateKey())))
+        );
+        setPrivateKeyImportError(false);
+      } catch (e) {
+        setPrivateKeyImportError(true);
+      }
+    }
+  }
+
   function clearKeysFromContext() {
     setContextPrivateKey("");
     setContextPublicKey("");
@@ -22,19 +42,35 @@ export default function ImportKeypair() {
       <header>Import key pair</header>
       <label>
         Your private key
-        <input value={contextPrivateKey()} />
+        <input ref={privateKeyInputField} value={contextPrivateKey()} />
       </label>
+      <Show when={privateKeyImportError()}>
+        <p class="pico-color-red-500">
+          <i>
+            Could not retrieve the public key. Please check the private key and
+            try again.
+          </i>
+        </p>
+      </Show>
       <label>
         Your public key
         <input value={contextPublicKey()} disabled />
       </label>
       <div role="group">
-        <button>Import key</button>
-        <button class="secondary">Eject keys</button>
+        <button onclick={importKeypair} disabled={contextPublicKey() !== ""}>
+          Import key
+        </button>
+        <button
+          onclick={clearKeysFromContext}
+          disabled={contextPublicKey() === ""}
+          class="secondary"
+        >
+          Eject keys
+        </button>
       </div>
       <footer>
         Be aware! Keys are not persisted and will be ejected when you refresh
-        the page
+        the page.
       </footer>
     </article>
   );
